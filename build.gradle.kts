241e895e734b5337e5e07af8ca446a0e12046552
Ramdani <m.ramdani@appsku.co.id>
2026-04-07 01:03:10 +0700

    synchronize build system and stabilize library versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,18 +6,3 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.compose.compiler) apply false
 }
-
-// build.gradle.kts (Project Level)
-allprojects {
-    configurations.all {
-        resolutionStrategy.eachDependency {
-            if (requested.group == "org.jetbrains.kotlin") {
-                useVersion("2.1.10")
-            }
-            // Tambahkan ini juga untuk KSP
-            if (requested.group == "com.google.devtools.ksp") {
-                useVersion("2.1.10-1.0.30")
-            }
-        }
-    }
-}
